c404d9fffcc2871d31465dc8224c4afccfa62f46
Wendeley Fernandes <wendeleyti@gmail.com>
2019-08-27 16:30:43 -0300

    Inserindo script de buscar de recursos no banco de dados

--- R/processamento_transferencias.R
+++ R/processamento_transferencias.R
@@ -33,9 +33,32 @@ salvar_rds_recursos_pb_enxuto <- function(arquivo, caminho){
 inserir_recursos_no_banco_de_dados <- function(recursos){
   dados <- recursos
   conexao <- conectarPostgreSql()
-  dbWriteTable(conexao, "recursos_portal_transferencias", recursos, row.names = FALSE, overwrite = TRUE)
-  dbSendQuery(conexao, "ALTER TABLE recursos_portal_transferencias ADD PRIMARY KEY (id_recurso)")
+  dbWriteTable(conexao, "recursos_portal_transparencia", recursos, row.names = FALSE, overwrite = TRUE)
+  dbSendQuery(conexao, "ALTER TABLE recursos_portal_transparencia ADD PRIMARY KEY (id_recurso)")
   dbDisconnect(conexao)
+}
+
+# Buscando o dataframe de recursos no banco de dados
+buscar_recursos_no_banco_de_dados <- function(){
+  conexao <- conectarPostgreSql()
+  recursos_tbl <- tbl(conexao, "recursos_portal_transparencia")
+  recursos <- recursos_tbl %>%
+    collect()
+  dbDisconnect(conexao)
+  recursos
+}
+
+buscar_total_recursos_por_municipio <- function(){
+  conexao <- conectarPostgreSql()
+  recursos_tbl <- tbl(conexao, "recursos_portal_transparencia")
+  total_recursos <- recursos_tbl %>%
+    group_by(nome_municipio) %>%
+    summarise(total_transferido = sum(valor_transferido, na.rm = TRUE)) %>%
+    arrange(desc(total_transferido)) %>%
+    collect()
+  dbDisconnect(conexao)
+  total_recursos$nome_municipio[total_recursos$nome_municipio == ''] <- 'GOVERNO DA PARAÍBA'
+  total_recursos
 }
 
 # INÍCIO #
@@ -44,7 +67,8 @@ recursos_clean <- ler_rds_recursos_pb("data/transf_pb.rds")
 recursos <- tratar_rds_recursos_pb(recursos_clean)
 inserir_recursos_no_banco_de_dados(recursos)
 salvar_rds_recursos_pb_enxuto(recursos, 'data/recursos_pb_enxuto.rds')
-
-
+recursos <- buscar_recursos_no_banco_de_dados()
 caminho_rds_recursos <- "data/recursos_pb_enxuto.rds"
 recursos_pb <- readRDS(caminho_rds_transferencias)
+
+total <- buscar_total_recursos_por_municipio()
