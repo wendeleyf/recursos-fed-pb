a84aa44a9a3a446b4e4f200e45afa8cbcfa01c03
CalixtoNeto <joaocalixto2@hotmail.com>
2019-08-30 16:56:25 -0300

    Adicionando grafico recurso função

--- R/processar_graficos.R
+++ R/processar_graficos.R
@@ -17,12 +17,12 @@ source('R/busca_recursos_no_bd.R')
 ## Sumário de Funções
 ##
 ## grafico_barra_tipo
-## buscar_total_recursos_por_municipio
+## grafico_barra_funcao
 ## 
 ## -------------------------------------------
 
 # -
-# Retorna os grafico de barras tipo de recursos federais salvos na base de dados 
+# Retorna os grafico de barras  de recursos federais por tipo salvos na base de dados 
 # @return {grafico_barra_tipo} recursos - grafico_barra com informações da base de dados
 # - 
 
@@ -33,17 +33,60 @@ grafico_barra_tipo <- function(){
     summarise(total = sum(valor_transferido))
   
   #gerando grafico
-  
-  p_total_tipo <- plot_ly(df_ano_tipo,x = ~ano , y = ~total, name = ~tipo_transferencia )%>%
+  p_total_tipo <- plot_ly(df_ano_tipo,
+                          x = ~ano , 
+                          y = ~total, 
+                          name = ~tipo_transferencia,
+                          text = ~paste("Ano :",ano,'<br>Total:R$',formatar(total)),
+                          hoverinfo = 'text')%>%
     layout(title = "Recursos Tipo Por Ano",
            yaxis = list(title = ~`total`,type = "log"),
            xaxis = list(title = ~tipo_transferencia),
            barmode = 'group')
   
+  #retornar grafico
   p_total_tipo 
   
   
 }
 
+# -
+# Retorna os grafico de barras  de recursos federais por funçao salvos na base de dados 
+# @return {grafico_barra_função} recursos - grafico_barra com informações da base de dados
+# - 
+
+grafico_barra_funcao <- function(){
+  #agurpando dados
+  df_ano_funcao <- recursos %>%
+    group_by(ano,nome_funcao)%>%
+    summarise(total = sum(valor_transferido))
+  
+  #gerando grafico
+  p_total_nome <- plot_ly(df_ano_funcao,
+                          x= ~ano,
+                          y = ~total,
+                          name = ~nome_funcao,
+                          color= ~log10(total),
+                          marker = list(colorscale='Viridis',
+                                        reversescale = T),
+                          text = ~paste("Funçao:",nome_funcao,
+                                         '<br>Total:R$',formatar(total)),
+                          hoverinfo = 'text')%>%
+    layout(title = "Recursos Função Por Ano",
+           #legend = list(y = 0.5),
+           yaxis = list(title = ~total,type = "log",showticklabels = FALSE),
+           xaxis = list(title = ~nome_funcao),
+           barmode = 'group')%>%
+    hide_colorbar()
+    
+  #retornar grafico
+  p_total_nome
+           
+  
+}
+
 # INÍCIO
 grafico_barra_tipo()
+grafico_barra_funcao()
+
+
