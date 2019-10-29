6e7d1a2411febbee718a5e5cea0e708eef70b06e
CalixtoNeto <joaocalixto2@hotmail.com>
2019-10-29 16:05:13 -0300

    otimizando buscar fornecedores

--- R/processamento_pagamentos.R
+++ R/processamento_pagamentos.R
@@ -68,11 +68,52 @@ buscar_fornecedores_unicos_educacao <- function(){
     group_by(CPF_CNPJ) %>%
     summarise(TOTAL_PAGO = sum(PAGO), Contagem = length(unique(MUNICIPIO)))
   
+  pagamentos <- pagamentos_filtrados_despesa%>%
+    select(CPF_CNPJ, MUNICIPIO, PAGO)
+  
+
+      
   pagamentos_fornecedores <- left_join(
     fornecedores,
     pagamentos_filtrados_despesa,
     by = "CPF_CNPJ"
   )
+}
+
+buscar_fornecedores <- function(){
+  
+  caminho_pagamentos_rds <- "data/pagamentos_educacao.rds"
+  pagamentos_educacao <- readRDS(caminho_pagamentos_rds)
+  grupos_despesa <- c("Outras Despesas Correntes", "Investimentos", "Inversões Financeiras")
+  
+  pagamentos_filtrados_despesa <- pagamentos_educacao %>%
+    filter(GRUPO_DE_NATUREZA_DE_DESPESA %in% grupos_despesa)
+  
+  pagamentos_filtrados_despesa <- pagamentos_filtrados_despesa %>%
+    select(CPF_CNPJ,FORNECEDOR, MUNICIPIO, PAGO)
+  
+  fornecedores <- pagamentos_filtrados_despesa %>%
+    distinct(CPF_CNPJ, FORNECEDOR)
+  
+  fornecedores <- fornecedores[!duplicated(fornecedores$CPF_CNPJ), ]
+  
+  AGREGADO_PAGO <- aggregate(PAGO ~ CPF_CNPJ, pagamentos_filtrados_despesa, sum)
+  AGRREGADO_COUNT_MUNICIPIO <- aggregate(MUNICIPIO ~ CPF_CNPJ, pagamentos_filtrados_despesa, function(x) length(unique(x)))
+  
+  
+  AGREGADO <- left_join(
+    AGREGADO_PAGO,
+    AGRREGADO_COUNT_MUNICIPIO,
+    by = "CPF_CNPJ"
+  )
+  
+  AGREGADO <- left_join(
+    fornecedores,
+    AGREGADO    ,
+    by = "CPF_CNPJ"
+  )
+  
+  
 }
 
 
